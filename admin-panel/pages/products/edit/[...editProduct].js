feed6046ae62f7e7ed2d110a8cb3e5c876188a45
Julio Bermudez <julioph0n3@gmail.com>
2024-01-10 14:44:40 -0500

    working on products page

---
+++ admin-panel/pages/products/edit/[...editProduct].js
@@ -0,0 +1,10 @@
+import Layout from "@/components/Layout/Layout"
+
+
+export default function EditProductPage(){
+    return(
+        <Layout>
+            Edit Product Here
+        </Layout>
+    )
+}
