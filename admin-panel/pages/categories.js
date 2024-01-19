e77190f934f34503046bd4ff9cfef1083716b944
Julio Bermudez <julioph0n3@gmail.com>
2024-01-19 04:57:10 -0500

    added product session

---
+++ admin-panel/pages/categories.js
@@ -0,0 +1,9 @@
+import Layout from "@/components/Layout/Layout";
+
+export default function categories(){
+    return(
+        <Layout>
+            <h1>Categories</h1>
+        </Layout>
+    )
+}
