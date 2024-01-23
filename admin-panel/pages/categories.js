df68e2027c3725a774e27ce6d4cd2bbdee86d7c7
Julio Bermudez <julioph0n3@gmail.com>
2024-01-23 05:46:01 -0500

    added edit and delete buttons to categories component

--- admin-panel/pages/categories.js
+++ admin-panel/pages/categories.js
@@ -1,9 +1,122 @@
 import Layout from "@/components/Layout/Layout";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { withSwal } from "react-sweetalert2";
 
-export default function categories(){
-    return(
-        <Layout>
-            <h1>Categories</h1>
-        </Layout>
-    )
+function Categories({swal}) {
+  const [editedCategory, setEditedCategory] = useState(null);
+  const [name, setName] = useState("");
+  const [parentCategory, setParentCategory] = useState("");
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+  function fetchCategories() {
+    axios.get("api/categories").then((result) => {
+      setCategories(result.data);
+    });
+  }
+  async function saveCategory(ev) {
+    ev.preventDefault();
+    const data = { name, parentCategory };
+    if (editedCategory) {
+      data._id = editedCategory._id;
+      await axios.put("/api/categories", data);
+      setEditedCategory(null);
+    } else {
+      await axios.post("/api/categories", data);
+    }
+    setName("");
+    fetchCategories();
+  }
+  function editCategory(category) {
+    setEditedCategory(category);
+    setName(category.name);
+    setParentCategory(category.parent?._id);
+  }
+  function deleteCategory(category) {
+    swal
+      .fire({
+        title: "Example",
+        text: "Hello World",
+        didOpen: () => {
+          // run when swal is opened...
+        },
+        didClose: () => {
+          // run when swal is closed...
+        },
+      })
+      .then((result) => {
+        // when confirmed and promise resolved...
+      })
+      .catch((error) => {
+        // when promise rejected...
+      });
+  }
+  return (
+    <Layout>
+      <h1>Categories</h1>
+      <label>
+        {editedCategory
+          ? `Edit category ${editedCategory.name}`
+          : "Create new category"}
+      </label>
+      <form onSubmit={saveCategory} className="flex gap-1">
+        <input
+          type="text"
+          placeholder={"Category name"}
+          value={name}
+          className="mb-0"
+          onChange={(ev) => setName(ev.target.value)}
+        />
+        <select
+          className="mb-0"
+          onChange={(ev) => setParentCategory(ev.target.value)}
+          value={parentCategory}
+        >
+          <option value="">No parent category</option>
+          {categories.length > 0 &&
+            categories.map((category) => (
+              <option value={category._id}>{category.name}</option>
+            ))}
+        </select>
+        <button type="submit" className="btn-primary py-1">
+          Save
+        </button>
+      </form>
+      <table className="basic mt-4">
+        <thead>
+          <tr>
+            <td>Category name</td>
+            <td>Parent category</td>
+            <td></td>
+          </tr>
+        </thead>
+        <tbody>
+          {categories.length > 0 &&
+            categories.map((category) => (
+              <tr key={category.id}>
+                <td>{category.name}</td>
+                <td>{category?.parent?.name}</td>
+                <td>
+                  <button
+                    className="btn-primary mr-1"
+                    onClick={() => editCategory(category)}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className="btn-primary"
+                    onClick={() => deleteCategory(category)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </Layout>
+  );
 }
+export default withSwal(({ swal }, ref) => <Categories swal={swal} />);
